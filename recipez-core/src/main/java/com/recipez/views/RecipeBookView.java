1516deb9e11ba2b6a49e2d7e498576de0b19f005
BearXDown <148283386+BearXDown@users.noreply.github.com>
2025-04-17 14:12:13 -0600

    adjustmants to view button

--- recipez-core/src/main/java/com/recipez/views/RecipeBookView.java
+++ recipez-core/src/main/java/com/recipez/views/RecipeBookView.java
@@ -20,6 +20,11 @@ import com.recipez.util.Observer;
 import com.recipez.util.Subject;
 import com.recipez.views.view_models.RecipeBookViewModel;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
+
 //This extends StackPane, but you can change it to whatever UI element works best for your View Layout.
 public class RecipeBookView extends StackPane implements Observer {
 
@@ -45,6 +50,7 @@ public class RecipeBookView extends StackPane implements Observer {
        loadRecipes();
        
     }
+}
 
     public void createView(){
         //intialize the properties and Create the UI for recipe Book here. Any data you need to 
@@ -70,10 +76,10 @@ public class RecipeBookView extends StackPane implements Observer {
 
         tableView.getColumns().add(nameColumn);
 
+        addViewButtonToTable();
+
         
         tableView.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
-
-        addViewButtonToTable();
         
         vboxRecipeBookContainer = new VBox(hboxRecipeBook, tableView);
         this.getChildren().add(vboxRecipeBookContainer);
@@ -82,26 +88,31 @@ public class RecipeBookView extends StackPane implements Observer {
     private void addViewButtonToTable() {
     TableColumn<Recipe, Void> colBtn = new TableColumn<>("Action");
 
-    colBtn.setCellFactory(param -> new TableCell<>() {
-        private final Button btn = new Button("View");
-
-        {
-            btn.setOnAction(event -> {
-                Recipe selectedRecipe = getTableView().getItems().get(getIndex());
-                openRecipeInView(selectedRecipe);
-            });
-        }
-
+    Callback<TableColumn<Recipe, Void>, TableCell<Recipe, Void>> cellFactory = new Callback<>() {
         @Override
-        protected void updateItem(Void item, boolean empty) {
-            super.updateItem(item, empty);
-            if (empty) {
-                setGraphic(null);
-            } else {
-                setGraphic(btn);
-            }
+        public TableCell<Recipe, Void> call(final TableColumn<Recipe, Void> param) {
+            return new TableCell<>() {
+                private final Button btn = new Button("View");
+
+                {
+                    btn.setOnAction(event -> {
+                        Recipe selectedRecipe = getTableView().getItems().get(getIndex());
+                        openRecipeInView(selectedRecipe);
+                    });
+                }
+
+                @Override
+                protected void updateItem(Void item, boolean empty) {
+                    super.updateItem(item, empty);
+                    setGraphic(empty ? null : btn);
+                }
+            };
         }
-    });
+    };
+
+    colBtn.setCellFactory(cellFactory);
+    tableView.getColumns().add(colBtn);
+}
 
     tableView.getColumns().add(colBtn);
 
